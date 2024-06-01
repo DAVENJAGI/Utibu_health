44a4651086c7685306e310259cb307b247559da7
robocode 6 <robocode@robocode.robocode.com>
2024-06-01 10:41:55 -0400

    login Endpoint change; Function Comments

--- api/v1/views/login.py
+++ api/v1/views/login.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Creates the first route, /users"""
+"""This is a route to handle the login request"""
 
 from api.v1.views import app_views
 from flask import jsonify, Blueprint, abort, request, make_response
@@ -11,15 +11,18 @@ import json
 
 @app_views.route("/login", strict_slashes=False, methods=["POST"])
 def login():
-    """get all users"""
-    data = request.get_json()
+    """A post request sent with email and password from the login page
+        Handles login for the app
+    """
+    data = request.get_json() # the request sent from the login page
 
     if not data or 'email' not in data or 'password' not in data:
         return make_response(jsonify({"error": "Missing data in request"}), 400)  
-    
+
     email = data['email']
     password = data.get('password')
-    
+
+# The data is sorted in the database, using the email and checks the password. 
     user = storage.getLogin(User, email)
     if not user:
         return make_response(jsonify({"error": "User not found"}), 401)
@@ -27,5 +30,6 @@ def login():
     if user.password != password:
         return make_response(jsonify({"error": "Invalid password"}), 401)
 
+# returns the object in dictionary format, associated with the email.
     return (jsonify({"message": "Login sucessful", "user": user.to_dict()}), 200)
 
