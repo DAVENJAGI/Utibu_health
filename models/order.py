765e568d746a196103a9f6c3d3d12a9b4b4d3452
robocode 6 <robocode@robocode.robocode.com>
2024-07-19 08:21:55 -0400

    admin_front_end, order model

---
+++ models/order.py
@@ -0,0 +1,35 @@
+#!usr/bin/python3
+"""This is a class containing the user  details"""
+import models
+from models.base_model import BaseModel, Base
+from os import getenv
+import sqlalchemy
+from sqlalchemy import Column, String, ForeignKey, Table, Integer
+from sqlalchemy.orm import relationship
+from models.doctor import Doctor
+from models.hospital import Hospital
+from models.user import User
+from models.medication import Medication
+
+
+class Order(BaseModel, Base):
+    """Represents order details"""
+    if models.storage_type == "db":
+        __tablename__ = 'orders'
+        doctor_id = Column(String(60), ForeignKey('doctors.id'), nullable=False)
+        medication_id = Column(String(60), ForeignKey('medication.id'), nullable=False)
+        user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
+        delivery_mode = Column(String(128), default="Pickup from hospital")
+        billing_cost = Column(Integer, nullable=False)
+        order_status = Column(String(60), nullable=False, default="pending approval")
+        quantity = Column(Integer, nullable=False)
+ 
+#        user = relationship("User", backref="appointment") 
+#         doctor = relationship("Doctor", backref="appointment")
+    else:
+        doctor_id = ""
+        hospital_id = ""
+        user_id = ""
+        description = ""
+        time = ""
+        date = ""
