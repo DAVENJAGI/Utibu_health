31b097b6958ace5726cc0303cb42c48e951bac6d
robocode 6 <robocode@robocode.robocode.com>
2024-08-22 19:31:39 -0400

    Athorization; a file that handles all the authentication

---
+++ models/authorization.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python3
+"""Creates the authentication functions.
+They act as authenticatiors for the function. They incude cookie session token and authorization token. Cookie session token expires after one hour"""
+
+from api.v1.views import app_views
+from flask import jsonify, Blueprint, abort, request, make_response, session
+from models import storage
+from models.user import User
+from models.disease import Disease
+from models.user_session import userSession
+from models.admin_session import adminSession
+from models.doctor_session import doctorSession
+import json
+from datetime import datetime
+
+from functools import wraps
+import secrets
+# user_view = Blueprint("users", __name__)
+
+#decorator to protect the endpoints
+
+def require_user_auth(f):
+    '''ensure's the endpoint  is authorized by auth_header for the user and has a valid session_id'''
+    @wraps(f)
+    def dec_func(*args, **kwargs):
+        '''auth header checking'''
+        auth_token = request.headers.get('X-Custom-Token')
+        print(f"received auth_token: {auth_token}")
+        if not auth_token or len(auth_token) != 48:
+            return make_response(jsonify({"Message": "Sorry, you do not have the valid authorization to perform the operation"}), 403)
+
+        session_id = request.cookies.get('session_id')
+        if not session_id:
+            return make_response(jsonify({"Message": "Session id missing."}), 403)
+
+        user_session = storage.get_session(userSession, session_id)
+        if not user_session:
+            return make_response(jsonify({"Message": "Invalid session"}), 401)
+#       print(user_session)
+        if user_session.expires_at <= datetime.utcnow():
+            return make_response(jsonify({"Message": "Expired session. Log in to continue"}), 401)
+        if user_session.authorization_token != auth_token:
+            return make_response(jsonify({"Message": "The Authentication token doesn't match the one generated for the session"}), 401) 
+
+        return f(*args, **kwargs)
+    return dec_func
+
+
+def require_admin_auth(f):
+    '''ensure's the endpoint  is authorized by auth_header for the admin and has a valid session_id'''
+    @wraps(f)
+    def dec_func(*args, **kwargs):
+        '''auth header checking'''
+        auth_token = request.headers.get('X-Custom-Token')
+        print(f"received auth_token: {auth_token}")
+        if not auth_token or len(auth_token) != 64:
+            return make_response(jsonify({"Message": "Sorry, you do not have the valid authorization to perform the operation"}), 403)
+
+        session_id = request.cookies.get('session_id')
+        if not session_id:
+            return make_response(jsonify({"Message": "Session id missing."}), 403)
+
+        admin_session = storage.get_session(adminSession, session_id)
+        if not admin_session:
+            return make_response(jsonify({"Message": "Invalid session"}), 401)
+#       print(user_session)
+        if admin_session.expires_at <= datetime.utcnow():
+            return make_response(jsonify({"Message": "Expired session. Log in to continue"}), 401)
+        if admin_session.authorization_token != auth_token:
+            return make_response(jsonify({"Message": "The Authentication token doesn't match the one generated for the session"}), 401)
+
+        return f(*args, **kwargs)
+    return dec_func
+
+def require_doctor_auth(f):
+    '''ensure's the endpoint  is authorized by auth_header for the doctor and has a valid session_id'''
+    @wraps(f)
+    def dec_func(*args, **kwargs):
+        '''auth header checking'''
+        auth_token = request.headers.get('X-Custom-Token')
+        print(f"received auth_token: {auth_token}")
+        if not auth_token or len(auth_token) != 48:
+            return make_response(jsonify({"Message": "Sorry, you do not have the valid authorization to perform the operation"}), 403)
+
+        session_id = request.cookies.get('session_id')
+        if not session_id:
+            return make_response(jsonify({"Message": "Session id missing."}), 403)
+
+        dkt_session = storage.get_session(doctorSession, session_id)
+        if not dkt_session:
+            return make_response(jsonify({"Message": "Invalid session"}), 401)
+#       print(user_session)
+        if dkt_session.expires_at <= datetime.utcnow():
+            return make_response(jsonify({"Message": "Expired session. Log in to continue"}), 401)
+
+        return f(*args, **kwargs)
+    return dec_func
+
+
+# HANDLES SECURITY FOR ENDPOINTS THAT ENSURE DATA BETWEEN ADMIN, DOCTOR, USER
+def verify_user_session(auth_token, session_id):
+    user_session = storage.get_session(userSession, session_id)
+    if not user_session or user_session.expires_at <= datetime.utcnow() or user_session.authorization_token != auth_token:
+        return False
+    return True
+
+def verify_admin_session(auth_token, session_id):
+    admin_session = storage.get_session(adminSession, session_id)
+    if not admin_session or admin_session.expires_at <= datetime.utcnow() or admin_session.authorization_token != auth_token:
+        return False
+    return True
+
+def require_user_or_admin_auth(f):
+    @wraps(f)
+    def wrapper(*args, **kwargs):
+        auth_token = request.headers.get('X-Custom-Token')
+        session_id = request.cookies.get('session_id')
+
+        if not auth_token or not session_id:
+            return make_response(jsonify({"Message": "Missing credentials"}), 403)
+
+        if verify_user_session(auth_token, session_id) or verify_admin_session(auth_token, session_id):
+            return f(*args, **kwargs)
+
+        return make_response(jsonify({"Message": "Sorry, you do not have valid authorization to access this data"}), 401)
+    return wrapper
