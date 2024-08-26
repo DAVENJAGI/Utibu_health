36ac03d5d7ecaa606182a7340b4711e2081c4976
robocode 6 <robocode@robocode.robocode.com>
2024-08-25 22:31:27 -0400

    using X-Custom-Header to authenticate

--- models/authorization.py
+++ models/authorization.py
@@ -28,18 +28,15 @@ def require_user_auth(f):
         if not auth_token or len(auth_token) != 48:
             return make_response(jsonify({"Message": "Sorry, you do not have the valid authorization to perform the operation"}), 403)
 
-        session_id = request.cookies.get('session_id')
-        if not session_id:
-            return make_response(jsonify({"Message": "Session id missing."}), 403)
 
-        hashed_session_id = hashlib.sha256(session_id.encode()).hexdigest()
-        user_session = storage.get_session(userSession, hashed_session_id)
+        hashed_auth_token = hashlib.sha256(auth_token.encode()).hexdigest()
+        user_session = storage.get_session(userSession, hashed_auth_token)
         if not user_session:
             return make_response(jsonify({"Message": "Invalid session"}), 401)
 #       print(user_session)
         if user_session.expires_at <= datetime.utcnow():
             return make_response(jsonify({"Message": "Expired session. Log in to continue"}), 401)
-        if user_session.authorization_token != auth_token:
+        if user_session.authorization_token != hashed_auth_token:
             return make_response(jsonify({"Message": "The Authentication token doesn't match the one generated for the session"}), 401) 
 
         return f(*args, **kwargs)
@@ -55,21 +52,16 @@ def require_admin_auth(f):
         print(f"received auth_token: {auth_token}")
         if not auth_token or len(auth_token) != 64:
             return make_response(jsonify({"Message": "Sorry, you do not have the valid authorization to perform the operation"}), 403)
+        
+        hashed_auth_token = hashlib.sha256(auth_token.encode()).hexdigest()
 
-
-        session_id = request.cookies.get('session_id')
-        if not session_id:
-            return make_response(jsonify({"Message": "Session id missing."}), 403)
-
-        hashed_session_id = hashlib.sha256(session_id.encode()).hexdigest()
-
-        admin_session = storage.get_session(adminSession, hashed_session_id)
+        admin_session = storage.get_session(adminSession, hashed_auth_token)
         if not admin_session:
             return make_response(jsonify({"Message": "Invalid session"}), 401)
 #       print(user_session)
         if admin_session.expires_at <= datetime.utcnow():
             return make_response(jsonify({"Message": "Expired session. Log in to continue"}), 401)
-        if admin_session.authorization_token != auth_token:
+        if admin_session.authorization_token != hashed_auth_token:
             return make_response(jsonify({"Message": "The Authentication token doesn't match the one generated for the session"}), 401)
 
         return f(*args, **kwargs)
@@ -85,41 +77,39 @@ def require_doctor_auth(f):
         if not auth_token or len(auth_token) != 48:
             return make_response(jsonify({"Message": "Sorry, you do not have the valid authorization to perform the operation"}), 403)
 
-        session_id = request.cookies.get('session_id')
-        if not session_id:
-            return make_response(jsonify({"Message": "Session id missing."}), 403)
-
-        hashed_session_id = hashlib.sha256(session_id.encode()).hexdigest()
-        dkt_session = storage.get_session(doctorSession, hashed_session_id)
+        hashed_auth_token = hashlib.sha256(auth_token.encode()).hexdigest()
+        dkt_session = storage.get_session(doctorSession, hashed_auth_token)
         if not dkt_session:
             return make_response(jsonify({"Message": "Invalid session"}), 401)
 #       print(user_session)
+        if dkt.authorization_token != hashed_auth_token:
+            return make_response(jsonify({"Message": "The authorization token does not match"}), 401)
         if dkt_session.expires_at <= datetime.utcnow():
             return make_response(jsonify({"Message": "Expired session. Log in to continue"}), 401)
-
+        
         return f(*args, **kwargs)
     return dec_func
 
 
 # HANDLES SECURITY FOR ENDPOINTS THAT ENSURE DATA BETWEEN ADMIN, DOCTOR, USER
-def verify_user_session(auth_token, session_id):
-    hashed_session_id = hashlib.sha256(session_id.encode()).hexdigest()
-    user_session = storage.get_session(userSession, hashed_session_id)
-    if not user_session or user_session.expires_at <= datetime.utcnow() or user_session.authorization_token != auth_token:
+def verify_user_session(auth_token):
+    hashed_auth_token = hashlib.sha256(auth_token.encode()).hexdigest()
+    user_session = storage.get_session(userSession, hashed_auth_token)
+    if not user_session or user_session.expires_at <= datetime.utcnow() or user_session.authorization_token != hashed_auth_token:
         return False
     return True
 
-def verify_admin_session(auth_token, session_id):
-    hashed_session_id = hashlib.sha256(session_id.encode()).hexdigest()
-    admin_session = storage.get_session(adminSession, hashed_session_id)
-    if not admin_session or admin_session.expires_at <= datetime.utcnow() or admin_session.authorization_token != auth_token:
+def verify_admin_session(auth_token):
+    hashed_auth_token = hashlib.sha256(auth_token.encode()).hexdigest()
+    admin_session = storage.get_session(adminSession, hashed_auth_token)
+    if not admin_session or admin_session.expires_at <= datetime.utcnow() or admin_session.authorization_token != hashed_auth_token:
         return False
     return True
 
-def verify_doctor_session(auth_token, session_id):
-    hashed_session_id = hashlib.sha256(session_id.encode()).hexdigest()
-    dkt_session = storage.get_session(doctorSession, hashed_session_id)
-    if not dkt_session or dkt_session.expires_at <= datetime.utcnow() or dkt_session.authorization_token != auth_token:
+def verify_doctor_session(auth_token):
+    hashed_auth_token = hashlib.sha256(auth_token.encode()).hexdigest()
+    dkt_session = storage.get_session(doctorSession, hashed_auth_token)
+    if not dkt_session or dkt_session.expires_at <= datetime.utcnow() or dkt_session.authorization_token != hashed_auth_token:
         return False
     return True
 
@@ -129,12 +119,12 @@ def require_user_or_admin_auth(f):
     def wrapper(*args, **kwargs):
         auth_token = request.headers.get('X-Custom-Token')
         session_id = request.cookies.get('session_id')
-        hashed_session_id = hashlib.sha256(session_id.encode()).hexdigest()
+        hashed_auth_token = hashlib.sha256(auth_token.encode()).hexdigest()
 
-        if not auth_token or not session_id:
+        if not auth_token:
             return make_response(jsonify({"Message": "Missing credentials"}), 403)
 
-        if verify_user_session(auth_token, session_id) or verify_admin_session(auth_token, session_id):
+        if verify_user_session(auth_token) or verify_admin_session(auth_token):
             return f(*args, **kwargs)
 
         return make_response(jsonify({"Message": "Sorry, you do not have valid authorization to access this data"}), 401)
@@ -147,10 +137,10 @@ def require_doctor_or_admin_auth(f):
         auth_token = request.headers.get('X-Custom-Token')
         session_id = request.cookies.get('session_id')
 
-        if not auth_token or not session_id:
+        if not auth_token:
             return make_response(jsonify({"Message": "Missing credentials"}), 403)
 
-        if verify_doctor_session(auth_token, session_id) or verify_admin_session(auth_token, session_id):
+        if verify_doctor_session(auth_token) or verify_admin_session(auth_token):
             return f(*args, **kwargs)
 
         return make_response(jsonify({"Message": "Sorry, you do not have valid authorization to access this data"}), 401)
@@ -161,13 +151,14 @@ def require_doctor_or_admin_or_user_auth(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
         auth_token = request.headers.get('X-Custom-Token')
-        session_id = request.cookies.get('session_id')
+#        session_id = request.cookies.get('session_id')
 
-        if not auth_token or not session_id:
+        if not auth_token:
             return make_response(jsonify({"Message": "Missing credentials"}), 403)
 
-        if verify_doctor_session(auth_token, session_id) or verify_user_session(auth_token, session_id) or verify_admin_session(auth_token, session_id):
+        if verify_doctor_session(auth_token) or verify_user_session(auth_token) or verify_admin_session(auth_token):
             return f(*args, **kwargs)
+
 
         return make_response(jsonify({"Message": "Sorry, you do not have valid authorization to access this data"}), 401)
     return wrapper
